09d14274b4223079d03d946034b158983e34ccc5
Lavina Mall <lavinamall94@gmail.com>
2017-05-20 13:00:47 +0530

    Add files via upload

---
+++ SquareOfNumbers.py
@@ -0,0 +1,14 @@
+#Program to calculate Squares of Numbers
+
+#Calculate square from 1 till the user input number
+def SquareOfNumbers(n):
+	while (n > 100 or n <= 0):
+		n = eval(input("Please enter a number between 1-100:"))
+	print ("The Squares are: ")
+	for i in range (1, n+1):
+		dict = {i: i**2}
+		print (dict)
+	return
+
+n = eval(input("Enter a Number (1-100): ")) # n = The user input
+SquareOfNumbers(n)
